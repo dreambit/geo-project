894e379ef39c00ab1f64cee9237137c4868fd3e9
dreambitc <dreambitc@gmail.com>
2015-01-13 00:27:53 +0300

    Scrollable, zoomable map proto.

--- UI/app/scripts/directives/map.js
+++ UI/app/scripts/directives/map.js
@@ -6,34 +6,43 @@
  * @description
  * # map
  */
-angular.module('geoApp')
-  .directive('map', function ($timeout, Svg, $compile) {
-    return {
-      templateUrl: 'views/components/map.html',
-      restrict: 'E',
-      scope: {
-        location: '=',
-        quality: '=',
-        showPopulation: '='
+ angular.module('geoApp')
+ .directive('map', function ($timeout, Svg, $compile) {
+  return {
+    templateUrl: 'views/components/map.html',
+    restrict: 'E',
+    scope: {
+      location: '=',
+      quality: '=',
+      showPopulation: '='
+    },
+    link: function ($scope, element) {
+      var createUrl = function(location, quality) {
+        return 'maps/' + (location || $scope.location) + (quality || $scope.quality) + '.svg';
       },
-      link: function ($scope, element) {
-        var createUrl = function(location, quality) {
-              return 'maps/' + (location || $scope.location) + (quality || $scope.quality) + '.svg';
-            },
+
+            /**
+
+            */
             initPopulation = function () {
               var svg = d3.select(svgDocument).select("svg"),
-                  center;
+              g = svg.select('g'),
+              center;
 
               svg.selectAll(".land").each(function () {
                 center = Svg.getCenter(this);
-                svg.append("circle").attr('cx', center.x)
-                                    .attr('cy', center.y)
-                                    .attr('class', 'population')
-                                    .attr('fill-opacity', 0.5)
-                                    .attr('r', Math.floor(Math.random() * 50) + 10)
-                                    .attr('fill', '#eea638'); 
+                g.append("circle").attr('cx', center.x)
+                .attr('cy', center.y)
+                .attr('class', 'population')
+                .attr('fill-opacity', 0.5)
+                .attr('r', Math.floor(Math.random() * 50) + 10)
+                .attr('fill', '#eea638'); 
               });
             },
+
+            /**
+
+            */
             populationVisibility = function (visible) {
               var svg = d3.select(svgDocument).select("svg");
 
@@ -41,24 +50,34 @@ angular.module('geoApp')
                 d3.select(this).attr("visibility", visible ? 'visible' : 'hidden');
               });
             },
+
+            /**
+
+            */
             onSVGReady = function (event) {
               svgDocument = event.target.getSVGDocument();
               initPopulation();
               populationVisibility($scope.showPopulation);
+
+              svgPanZoom('#map', {
+                zoomEnabled: true,
+                controlIconsEnabled: true
+              });
+
             },
             svgDocument;
 
-        $scope.$watch('location', function(location) {
-          $scope.url = createUrl(location);
-        });
+            $scope.$watch('location', function(location) {
+              $scope.url = createUrl(location);
+            });
 
-        $scope.$watch('quality', function(quality) {
-          $scope.url = createUrl(null, quality);
-        });
+            $scope.$watch('quality', function(quality) {
+              $scope.url = createUrl(null, quality);
+            });
 
-        $scope.$watch('showPopulation', function(showPopulation) {
-          populationVisibility(showPopulation);
-        });
+            $scope.$watch('showPopulation', function(showPopulation) {
+              populationVisibility(showPopulation);
+            });
 
         // on dom ready
         $timeout(function(){
